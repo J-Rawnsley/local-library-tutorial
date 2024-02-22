8697d94c3efda72e280438ab7a9c0c8862b65e1b
Joe Rawnsley <joe.rawnsley@gmail.com>
2024-02-22 14:53:46 +0000

    implemented create book instance form. updaed bookinstance controller and bookinstance model to add virtual date property

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,7 +1,10 @@
 const BookInstance = require('../models/bookinstance');
+const Book = require('../models/book');
+
 const asyncHandler = require('express-async-handler');
 const { book_detail } = require('./bookController');
 const { body, validationResult } = require('express-validator');
+const { format } = require('morgan');
 
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
   const allBookInstances = await BookInstance.find().populate('book').exec();
@@ -31,12 +34,57 @@ exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance create GET');
+  const allBooks = await Book.find({}, 'title').sort({ title: 1 }).exec();
+
+  res.render('bookinstance_form', {
+    title: 'Add Book Instance',
+    book_list: allBooks,
+  });
 });
 
-exports.bookinstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance create POST');
-});
+exports.bookinstance_create_post = [
+  body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must be at least 3 characters')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body('status').escape(),
+  body('due_back', 'Invalid date')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+  asyncHandler(async (req, res, next) => {
+
+    const errors = validationResult(req);
+    console.log(req.body)
+    console.log(validationResult(req))
+    
+
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      console.log("re-redering form...")
+      const allBooks = await Book.find({}, 'title').sort({ title: 1 }).exec();
+      
+      res.render('bookinstance_form', {
+        title: 'Add Book Instance',
+        book_list: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstnce: bookInstance,
+      });
+      return;
+    } else {
+      await bookInstance.save();
+      res.redirect(bookInstance.url);
+    }
+  }),
+];
 
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
   res.send('NOT IMPLEMENTED: BookInstance delete GET');
