a2b2960638a5c65afd642d8e808db8134a0b2e68
Joe Rawnsley <joe.rawnsley@gmail.com>
2024-04-09 15:14:07 +0100

    implemented book delete get and post route challenge

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -138,11 +138,36 @@ exports.book_create_post = [
 ];
 
 exports.book_delete_get = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Book delete GET');
+	const [book, allBookInstances] = await Promise.all([
+		Book.findById(req.params.id).populate('author').exec(),
+		BookInstance.find({ book: req.params.id }).exec(),
+	]);
+
+	res.render('book_delete', {
+		title: 'Delete Book',
+		book: book,
+    instances: allBookInstances,
+	});
 });
 
 exports.book_delete_post = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Book delete POST');
+  console.log("book delete post")
+	const [book, allBookInstances] = await Promise.all([
+    Book.findById(req.params.id).exec(),
+    BookInstance.find({ book: req.params.id}).exec(),
+  ])
+
+  if (allBookInstances.length > 0) {
+    res.render("book_delete", {
+      title: 'Delete Book',
+      book: book,
+      instances: allBookInstances,
+    })
+  } else {
+    console.log("book has no instances so should delete")
+    await Book.findByIdAndDelete(req.body.bookid);
+    res.redirect("/catalog/books")
+  }
 });
 
 exports.book_update_get = asyncHandler(async (req, res, next) => {
