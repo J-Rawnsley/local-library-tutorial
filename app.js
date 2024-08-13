52401abf2fdf2ac25cc60ba3d62a1fff098944cc
Joe Rawnsley <joe.rawnsley@gmail.com>
2024-08-13 12:21:08 +0100

    troubleshooting - changed order of require statements in app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const createError = require('http-errors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
